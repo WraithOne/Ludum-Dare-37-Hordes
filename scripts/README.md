9e91ac34e07e08a6c7fd28bc9e593ab0f205de23
WraithOne <WraithOne@Dark-Abyss.net>
2016-12-10 14:38:25 +0100

    Basic Level(Room)

---
+++ scripts/README.md
@@ -0,0 +1,4 @@
+# Ludum-Dare-37-Hordes
+
+babylon.max.js is a snapshot of https://github.com/BabylonJS/Babylon.js
+http://cdn.babylonjs.com/2-5/babylon.max.js
--- scripts/scene.js
+++ scripts/scene.js
@@ -1,25 +1,74 @@
-// Here begins a function that we will 'call' just after it's built
-      var createScene = function () {
-         // Now create a basic Babylon Scene object
-         var scene = new BABYLON.Scene(engine);
-         // Change the scene background color to green.
-         scene.clearColor = new BABYLON.Color3(0, 1, 0);
-         // This creates and positions a free camera
-         var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-         // This targets the camera to scene origin
-         camera.setTarget(BABYLON.Vector3.Zero());
-         // This attaches the camera to the canvas
-         camera.attachControl(canvas, false);
-         // This creates a light, aiming 0,1,0 - to the sky.
-         var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-         // Dim the light a small amount
-         light.intensity = .5;
-         // Let's try our built-in 'sphere' shape. Params: name, subdivisions, size, scene
-         var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-         // Move the sphere upward 1/2 its height
-         sphere.position.y = 1;
-         // Let's try our built-in 'ground' shape. Params: name, width, depth, subdivisions, scene
-         var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
-         // Leave this function
-         return scene;
-      }; // End of createScene function
+var createScene = function ()
+{
+    // Now create a basic Babylon Scene object
+    var scene = new BABYLON.Scene(engine);
+
+    // Enable Collisions
+    scene.collisionsEnabled = true;
+
+    // Change the scene background color to green.
+    scene.clearColor = new BABYLON.Color3(0, 1, 0);
+
+    // This creates and positions a free camera
+    var camera = new BABYLON.FreeCamera("FreeCamera", new BABYLON.Vector3(0, 5, 0), scene);
+    camera.attachControl(canvas, true);
+
+    //Then apply collisions and gravity to the active camera
+    camera.checkCollisions = true;
+    camera.applyGravity = true;
+    //Set the ellipsoid around the camera (e.g. your player's size)
+    camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
+
+    // This creates a light, aiming 0,1,0 - to the sky.
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+    // Dim the light a small amount
+    light.intensity = .5;
+
+    var roomSize = 60.0;
+    //Ground
+    var ground = BABYLON.Mesh.CreatePlane("ground", roomSize, scene);
+    ground.material = new BABYLON.StandardMaterial("groundMat", scene);
+    ground.material.diffuseColor = new BABYLON.Color3(1, 1, 1);
+    ground.material.backFaceCulling = false;
+    ground.position = new BABYLON.Vector3(0, 0, 0);
+    ground.rotation = new BABYLON.Vector3(Math.PI / 2, 0, 0);
+    ground.checkCollisions = true;
+
+    //Wall material
+    var wallMaterial = new BABYLON.StandardMaterial("wallMat", scene);
+    wallMaterial.diffuseColor = new BABYLON.Color3(1,0,0);
+    wallMaterial.backFaceCulling = false;
+
+    // Wall Front
+    var wallfront = BABYLON.Mesh.CreateBox("WallFront", roomSize, scene);
+    wallfront.material = wallMaterial;
+    wallfront.position = new BABYLON.Vector3(0, -20, roomSize);
+    wallfront.checkCollisions = true;
+    // Wall back
+    var wallback = BABYLON.Mesh.CreateBox("WallBack", roomSize, scene);
+    wallback.material = wallMaterial;
+    wallback.position = new BABYLON.Vector3(0, -20, -roomSize);
+    wallback.checkCollisions = true;
+    // Wall left
+    var wallleft = BABYLON.Mesh.CreateBox("WallLeft", roomSize, scene);
+    wallleft.material = wallMaterial;
+    wallleft.position = new BABYLON.Vector3(-roomSize, -20, 0);
+    wallleft.rotation = new BABYLON.Vector3(0, Math.PI / 2, 0);
+    wallleft.checkCollisions = true;
+    // Wall right
+    var wallright = BABYLON.Mesh.CreateBox("WallRight", roomSize, scene);
+    wallright.material = wallMaterial;
+    wallright.position = new BABYLON.Vector3(roomSize, -20, 0);
+    wallright.rotation = new BABYLON.Vector3(0, Math.PI / 2, 0);
+    wallright.checkCollisions = true;
+    // Register updateScene to execute before Render
+    scene.registerBeforeRender(updateScene);
+
+    // Leave this function
+    return scene;
+}; // End of createScene function
+
+updateScene = function()
+{
+
+};
